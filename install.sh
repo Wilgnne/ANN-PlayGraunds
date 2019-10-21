85f6320d08a8ea5b1aad0911feaa18927266a631
Wilgnne <w.khawanbarbosa@gmail.com>
2019-10-21 16:56:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo "Install Neural Network Playgraund - Wilgnne K."
+sudo rm -r /usr/lib/python3.7/NeuralNetwork;
 sudo rm -r /usr/lib/python3.7/site-packages/NeuralNetwork;
 
-sudo git clone https://github.com/Wilgnne/Learning-ANN.git /usr/lib/python3.7/site-packages/NeuralNetwork
+sudo git clone https://github.com/Wilgnne/Learning-ANN.git /usr/lib/python3.7/NeuralNetwork
 
-sudo git clone https://github.com/Wilgnne/ANN-PlayGraunds.git /usr/lib/python3.7/site-packages/NeuralNetwork/Playgraund
+sudo git clone https://github.com/Wilgnne/ANN-PlayGraunds.git /usr/lib/python3.7/NeuralNetwork/Playgraund
